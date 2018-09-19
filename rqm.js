79bbc01b6d9543d30e88920f1da089f17cd5cd53
Clint Jones <34784858+CubeNode@users.noreply.github.com>
2018-09-18 20:59:01 -0400

    Add files via upload

---
+++ rqm.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  //$('body').css('background-color', randomColor());
+
+  $.ajax({
+    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    success: function(q){
+      var post = q.shift();
+      $("#quote").html(post.content + "— " + post.title);
+    },
+    cache: false
+  });
+
+  $("#newquote").on("click", function(){
+    $.ajax({
+    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    success: function(q){
+      var post = q.shift();
+      $("#quote").html(post.content + "— " + post.title)
+    },
+      cache: false
+    });
+  });
+
+  $(".twitter-share").on("click", function()      {
+    $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + $('#quote').text());
+   });
+});
+
+var safeColors = ['00','33','66','99','cc','ff'];
+var rand = function() {
+    return Math.floor(Math.random()*6);
+};
+var randomColor = function() {
+    var r = safeColors[rand()];
+    var g = safeColors[rand()];
+    var b = safeColors[rand()];
+    return "#"+r+g+b;
+};
